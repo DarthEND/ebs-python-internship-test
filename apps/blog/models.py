30a0136fafc9094aa734e97b906528f5bb6186c5
Oleg Andronic <mdplaymc@gmail.com>
2026-08-01 11:58:23 +0300

    Added Comments model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -21,3 +21,11 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comments(models.Model):
+    text = models.TextField()
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
