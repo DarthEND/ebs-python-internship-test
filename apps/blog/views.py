5ca307e21f372ece671d9c9c563cdc4b5bf69cfd
Oleg Andronic <mdplaymc@gmail.com>
2026-08-01 11:38:00 +0300

    Added endpoint for creating blog posts

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,12 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.generics import GenericAPIView, get_object_or_404
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.request import Request
 from rest_framework.response import Response
 
 from apps.blog.models import Blog, Category
 from apps.blog.serializers import BlogSerializer, CategorySerializer
-from apps.common.permissions import ReadOnly
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -16,16 +15,22 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
 class BlogListView(GenericAPIView):
     serializer_class = BlogSerializer
-    permission_classes = (ReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get(self, request: Request) -> Response:
         blogs = Blog.objects.all()
         return Response(self.get_serializer(blogs, many=True).data)
 
+    def post(self, request: Request) -> Response:
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 class BlogItemView(GenericAPIView):
     serializer_class = BlogSerializer
-    permission_classes = (ReadOnly, IsAuthenticated)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get(self, request: Request, pk: int) -> Response:
         blog: Blog = get_object_or_404(Blog.objects.all(), pk=pk)
